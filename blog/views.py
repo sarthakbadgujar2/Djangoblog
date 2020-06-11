605a3a1ef13cb6f46064d58f6d78409ef9c632f0
Sarthak <sarthakbadgujar2@gmail.com>
2020-06-11 22:33:00 +0530

    Database created fetching the blogs from database

--- blog/views.py
+++ blog/views.py
@@ -1,25 +1,11 @@
 from django.shortcuts import render
+from . models import Post
 
-
-posts =[
-    {
-        'author' : 'sarthak',
-        'title'  : 'Blog post 1',
-        'content': 'First post content',
-        'date_posted'  : 'April 24,2020'
-    },
-      {
-        'author' : 'sarth',
-        'title'  : 'Blog post 2',
-        'content': 'second post content',
-        'date_posted'  : 'April 25,2020'
-    },
-]
 
 # Create your views here.
 def home(request):
     context ={
-        'posts' : posts
+        'posts' : Post.objects.all()
     }
     return render(request,'blog/home.html', context)  
 
