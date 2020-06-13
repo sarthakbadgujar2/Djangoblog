362b63099137df2b33a0b6e9a001b03abd23c7bd
Sarthak <sarthakbadgujar2@gmail.com>
2020-06-13 23:27:41 +0530

    specific post view page added using DetailView

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
-from . models import Post
+from django.views.generic import ListView, DetailView
+from .models import Post
 
 
 # Create your views here.
@@ -9,5 +10,17 @@ def home(request):
     }
     return render(request,'blog/home.html', context)  
 
+
+class PostListView(ListView):
+    model = Post
+    template_name = 'blog/home.html'   # <app>/<model>_<viewtype>/html
+    context_object_name = 'posts'
+    ordering = ['-date_posted']
+
+
+class PostDetailView(DetailView):
+    model = Post
+
+
 def about(request):
-    return render(request,'blog/about.html',{ 'title' : 'About' })  
+    return render(request, 'blog/about.html', { 'title' : 'About' })
