604c9548c82e29d48c80b4627e0242e8b257dd20
Sarthak <sarthakbadgujar2@gmail.com>
2020-06-16 22:44:06 +0530

    password reset added

--- socialsite/settings.py
+++ socialsite/settings.py
@@ -129,3 +129,11 @@ CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 LOGIN_REDIRECT_URL = 'blog-home'
 LOGIN_URL = 'login'
+
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+EMAIL_HOST_USER = 'USER EMAIL'
+EMAIL_HOST_PASS = ' PASSWORD'
